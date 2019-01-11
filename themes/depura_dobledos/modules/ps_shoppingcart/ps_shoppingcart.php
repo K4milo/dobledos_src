b8c736cc03b91da4ff68f315164980a7986bfe57
k4milo <kmilo.vanegas@gmail.com>
2019-01-11 07:29:38 -0500

    11/010/2019

--- themes/depura_dobledos/modules/ps_shoppingcart/ps_shoppingcart.php
+++ themes/depura_dobledos/modules/ps_shoppingcart/ps_shoppingcart.php
@@ -138,6 +138,7 @@ class Ps_Shoppingcart extends Module implements WidgetInterface
             parent::install()
                 && $this->registerHook('header')
                 && $this->registerHook('displayTop')
+                && $this->registerHook('displaySizesCms')
                 && Configuration::updateValue('PS_BLOCK_CART_AJAX', 1)
         ;
     }
